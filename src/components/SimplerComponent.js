dfaba13e51b4aaa4cdb9c332f71441b76482effb
Scott Waring <scott.waring@gmail.com>
2018-11-24 13:38:59 -0500

    Done.

--- src/components/SimplerComponent.js
+++ src/components/SimplerComponent.js
@@ -1,1 +1,12 @@
-// Code SimplerComponent Here
+import React, {Component} from 'react'
+
+class SimplerComponent extends React.Component {
+
+  render(){
+    return (
+      <div onClick={()=> this.props.handleClick}>I am just {this.props.mood}</div>
+    )
+  }
+}
+
+export default SimplerComponent;
--- src/index.js
+++ src/index.js
@@ -4,10 +4,20 @@ import ReactDOM from 'react-dom';
 import SimpleComponent from './components/SimpleComponent';
 import SimplerComponent from './components/SimplerComponent';
 
+let mood = 'happy'
+function changeMood() {
+  if (mood === 'happy') {
+    mood = 'sad'
+  } else {
+    mood = 'happy'
+  }
+}
+
+
 ReactDOM.render(
   <div>
     <SimpleComponent />
-    <SimplerComponent handleClick={undefined} />
+    <SimplerComponent mood={this.mood} handleClick={this.changeMood} />
   </div>,
   document.getElementById('root')
 );
