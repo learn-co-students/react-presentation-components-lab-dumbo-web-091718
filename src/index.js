9d0f175737c0744ef9ec3afc7e896410a36acbd3
Brian DiRito <bdirito@gmail.com>
2018-11-25 21:50:25 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import SimplerComponent from './components/SimplerComponent';
 ReactDOM.render(
   <div>
     <SimpleComponent />
-    <SimplerComponent handleClick={undefined} />
+    <SimplerComponent handleClick={console.log("Hey, you deserve to be happy!")} />
   </div>,
   document.getElementById('root')
 );
